5ea958128f5a700ff5703c9947e10d217fec8f38
sharon002 <sharonjep2016@gmail.com>
2021-05-30 21:45:47 +0300

    added generate  password

--- run.py
+++ run.py
@@ -107,7 +107,7 @@ def main():
                                 password = input('Enter your password: ').strip()
                                 break
                             elif psw_choice == 'gp':
-                                password = generate_password()
+                                password = generate_password(psk200)
                                 break
                             elif psw_choice == 'ex':
                                 break
